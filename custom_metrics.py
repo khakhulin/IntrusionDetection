5c7a2b86fd63f090bddff39548e7beabadf134aa
DenisKoposov <denis.koposov@phystech.edu>
2017-10-08 18:54:56 +0300

    precision, recall, f1 metrics are available in custom_metrics.py

---
+++ custom_metrics.py
@@ -0,0 +1,36 @@
+from keras import backend as K
+
+def recall(y_true, y_pred):
+        """Recall metric.
+
+        Only computes a batch-wise average of recall.
+
+        Computes the recall, a metric for multi-label classification of
+        how many relevant items are selected.
+        """
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+        recall = true_positives / (possible_positives + K.epsilon())
+        
+        return recall
+
+def precision(y_true, y_pred):
+        """Precision metric.
+
+        Only computes a batch-wise average of precision.
+
+        Computes the precision, a metric for multi-label classification of
+        how many selected items are relevant.
+        """
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        
+        return precision
+
+def f1(y_true, y_pred):
+
+    prec = precision(y_true, y_pred)
+    rec = recall(y_true, y_pred)
+    
+    return 2 * ((prec * rec) / (prec + rec))
